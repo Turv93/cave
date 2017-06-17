2d9bd0db154ab908ee8f1b51270a77ab7ed6e9ba
Turv93 <erlang871@gmail.com>
2017-06-17 16:07:44 +0300

    add calc

--- app/js/common.min.js
+++ app/js/common.min.js
@@ -46,5 +46,48 @@ $(function() {
 });
 
 $(document).ready(()=>{
+    parallaxInit();
+    sliderInit();
+    acmoInit();
+});
+
+
+function parallaxInit(){
     $('#diamonds').parallax();
-})
+}
+
+function acmoInit(){
+    $.ajax({
+        url: 'http://beta.json-generator.com/api/json/get/EyTiVz0GQ',
+        methd: 'GET'
+    }).done((data)=>{
+        debugger
+    })
+}
+
+function sliderInit(){
+    
+    let calcVals = {};
+    
+    let calc = (vals)=>{
+        let result = 0;
+        
+        result = (calcVals['r1'] + 1  - calcVals['r2']) * calcVals['r3'];
+
+        return result;
+    }
+    
+    let refresh = function(e,ui){
+            let val = $(this).slider('value') || 0;
+            calcVals[e.target.id] = val;
+            $('#i1').val(calc(calcVals) || 0);
+        };
+    
+    $('#r1, #r2, #r3').slider({
+        min: 5,
+        max: 205,
+        value: 15,
+        change: refresh,
+        create: refresh
+    });
+}
--- gulpfile.js
+++ gulpfile.js
@@ -27,6 +27,7 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+        'app/libs/jquery/jquery-ui.min.js',
         'app/libs/owl/owl.carousel.js',
         'app/libs/parallax/parallax.js',
 		'app/js/common.min.js', // Всегда в конце
